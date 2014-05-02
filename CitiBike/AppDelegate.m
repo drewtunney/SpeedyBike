6b378a14a1070dd8e870faff52bc091e879e9c94
drewtunney <bryantunney@gmail.com>
2014-05-02 14:43:27 -0400

    added google maps ios sdk

--- CitiBike/AppDelegate.m
+++ CitiBike/AppDelegate.m
@@ -7,12 +7,14 @@
 //
 
 #import "AppDelegate.h"
+#import <GoogleMaps/GoogleMaps.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    [GMSServices provideAPIKey:@"AIzaSyCpFcnTzlPxt1qDCH7rzf1sMs-5HKo8ZRk"];
     return YES;
 }
 							
---
+++ CitiBike/MapVC.h
@@ -0,0 +1,13 @@
+//
+//  MapVC.h
+//  CitiBike
+//
+//  Created by Drew Tunney on 5/2/14.
+//  Copyright (c) 2014 drewtunney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapVC : UIViewController
+
+@end
