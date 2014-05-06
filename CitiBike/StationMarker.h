0d067d82135fd62880d466548cbfa52822320e48
drewtunney <bryantunney@gmail.com>
2014-05-06 09:56:30 -0400

    added Station Marker Class

---
+++ CitiBike/StationMarker.h
@@ -0,0 +1,13 @@
+//
+//  StationMarker.h
+//  CitiBike
+//
+//  Created by Drew Tunney on 5/5/14.
+//  Copyright (c) 2014 drewtunney. All rights reserved.
+//
+
+#import <GoogleMaps/GoogleMaps.h>
+
+@interface StationMarker : GMSMarker
+
+@end
---
+++ CitiBike/StationMarker.m
@@ -0,0 +1,13 @@
+//
+//  StationMarker.m
+//  CitiBike
+//
+//  Created by Drew Tunney on 5/5/14.
+//  Copyright (c) 2014 drewtunney. All rights reserved.
+//
+
+#import "StationMarker.h"
+
+@implementation StationMarker
+
+@end
